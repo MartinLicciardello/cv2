87a0869da4996d61c285ba8fc6dd02eddd74de98
Martin <martinlicciardello7@gmail.com>
2022-01-22 21:18:17 -0300

    first commit

---
+++ javascript/components.js
@@ -0,0 +1,48 @@
+/*PROPÓSITO: Funcion que retorna una plantilla literal con los datos de las ceramicas enviadas por parametro.Cada *producto va a tener asignado un modal, a traves del cual se podra ver información mas detallada del producto seleccionado.*/
+
+ function componenteCard(producto){
+    return `<div class="card m-2">
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img src="${producto.imagen}" class="img-thumbnail">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title"><strong>${producto.nombre}</strong></h5>
+                    <button id=${producto.id} type="button" class="btn btn-primary btn-producto">Sumar</button>
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal${producto.id}">
+                    Info
+                    </button>
+                    <div class="modal fade" id="exampleModal${producto.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h3 class="modal-title" id="exampleModalLabel"><b>Ceramica</b></h3>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                        </div>
+                        <div class="modal-body">
+                        <h5 ><strong>Nombre:</strong>${producto.nombre}</h5>
+                        <h5 ><strong>Marca:</strong>${producto.marca}</h5>
+                        <h5 ><strong>Precio:</strong> $${producto.precio}</h5>
+                        <h5 ><strong>Cubre:</strong> ${producto.metrosCaja}mts²</h5>
+                        <h5 ><strong>Medida:</strong> ${producto.medida}</h5>
+                        <img src="${producto.imagen}" class="img-thumbnail">
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+                </div>
+              </div>
+            </div>`
+  }
+/*SE QUITARON PROPIEDADES IMPLEMNTADAS DESDE UN CSS, ESPECIFICO PARA CERAMICAS, PARA MEJORAR LA VISUALIDAD DESDE EL DOM*/ 
+  $("#bodyCeramica").css({ "margin-right":"auto",
+  "margin-left": "auto","background-image": "url(ImagenesPag/fondoPoniendoCeramica9.jpg)"})
+
+                      
---
+++ javascript/main.js
@@ -0,0 +1,91 @@
+
+//----------------------------------CLASES----------------------------------//
+class Producto {
+    constructor(datos){
+      this.id = datos.id;
+      this.nombre = datos.nombre;
+      this.moneda = datos.marca;
+      this.precio = datos.precio;
+      this.medida = datos.medida;
+      this.metrosCaja = datos.metrosCaja
+      this.cantidad = 1;
+    }
+    addCantidad(){
+      this.cantidad++;
+    }
+    getTotal(){
+      return (this.cantidad *  this.precio);
+    }
+    getTotalMetrosCuadrados(){
+      return(this.cantidad * this.metrosCaja);
+    }
+  }//dasd
+  //DOM CARGADO EFECTIVAMENTE
+  $(document).ready(function(){
+    console.log("Dom cargado");
+  })
+  //Almacenar datos con (clave,valor) de ceramicas
+
+ const guardarLocal = (clave,valor) =>{localStorage.setItem(clave,valor)};
+
+    //Recuperacion de datos
+    guardarLocal("listaProductos", JSON.stringify( DATOSCERAMICAS));
+
+    let muestraGuardarLocal = JSON.parse(localStorage.getItem("listaProductos"));
+    console.log(muestraGuardarLocal);
+
+  //----------------------------------FUNCIONES----------------------------------//
+  //AGREGAMOS UNA ESTRUCTURA HTML AL CONTENEDOR DE ID contenidoGenerado
+  function generarHTMLJQ(ceramica){
+    //UTILIZAMOS EL METODO DE jQUERY PREPEND PARA AGREGAR
+    $('#contenidoGenerado').prepend(componenteCard(ceramica))
+  }
+  //----------------------------------EVENTOS----------------------------------//
+  //ASOCIAMOS EN READY LOS EVENTOS SOBRE EL HTML GENERADO
+  $(document).ready(function () {
+    $(".btn-producto").click(function (e) { 
+      //VER SI EL PRODUCTO YA ESTA EN EL CARRITO CON FIND
+      let producto = SELECCIONADOS.find(producto => producto.id == e.target.id);
+      if(producto != undefined){
+        //ESTA EN EL CARRITO, ENTONCES NO ES NECESARIO BUSCAR LA INFORMACION EN EL ARRAY DE DATOS, SUMAMOS UNA CANTIDAD
+        producto.addCantidad();
+      }else{
+        //NO ESTA EN EL CARRITO, ENTONCES LO BUSCAMOS EN EL ARRAY DE DATOS
+        let seleccionado = DATOSCERAMICAS.find(producto => producto.id == e.target.id);
+        SELECCIONADOS.push(new Producto(seleccionado));
+      }
+      
+      //SALIDA DEL CARRITO SEGUN LA INFORMACION DEL ARRAY DE SELECCIONADOS.
+      
+      
+      $("#compras").empty();
+      for (const producto of SELECCIONADOS) {
+          $("#compras").append(`<h3><strong>${producto.nombre}</strong></h3> 
+          <h3>Cantidad ${producto.cantidad} Total: $${producto.getTotal()}</h3>
+          <h3>Metros totales: ${producto.getTotalMetrosCuadrados()}mts²</h3>`);
+      }
+    });
+  });
+  //----------------------------------PROGRAMA PRINCIPAL----------------------------------//
+  //POR CADA ELEMENTO EN "DATOSCERAMICAS" VAMOS A CREAR UNA ESTRUCTURA EN EL HTML ASOCIADA.
+  for(let ceramica of DATOSCERAMICAS){
+    //LLAMADA A LA FUNCIÓN DE generarHTML PARA CADA CERAMICA. PASAMOS LA INFORMACION DE "ceramica" POR PARÁMETRO.
+    generarHTMLJQ(ceramica);
+  }
+  //AGREGAR CLASE A ELEMENTO CON jQUERY
+  $('#contenidoGenerado').addClass("miNuevaClase");
+  
+  //MODAL
+  $('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
+
+
+
+//Implementamos el uso de AJAX, PARA LA FUNCIONALIDAD SINCRONICA UTILIZADA
+
+ $(document).ready($.getJSON("ceramica.json",function(datos,estado){
+    console.log(datos);
+    console.log(estado);
+  }))
+  
